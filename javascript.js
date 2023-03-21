56fd71d808475b7a890d98deb23d29a644ba77da
Tiago Tinoco <tiagotinocomartinsdossantos@gmail.com>
2023-03-21 17:17:37 -0300

    fix name

--- javascript.js
+++ javascript.js
@@ -17,7 +17,7 @@ var descriptionAna = "Tecido de algodão e renda com 5,8cm de largura. Acompanha
 
 function emily() {
     if (description.innerHTML === descriptionBianca || description.innerHTML === descriptionSophie || description.innerHTML === descriptionAlanis || description.innerHTML === descriptionAna) {
-        names.innerHTML = "Emily";
+        names.innerHTML = "Véu Emily";
         description.innerHTML = descriptionEmily;
         price.innerHTML = "R$ 60,00";
         image.src = "image/emily-veil.jpeg";
@@ -26,7 +26,7 @@ function emily() {
 
 function bianca() {
     if (description.innerHTML === descriptionEmily || description.innerHTML === descriptionSophie || description.innerHTML === descriptionAlanis || description.innerHTML === descriptionAna) {
-        names.innerHTML = "Bianca";
+        names.innerHTML = "Véu Bianca";
         description.innerHTML = descriptionBianca;
         price.innerHTML = "R$ 65,00";
         image.src = "image/bianca-veil.jpeg";
@@ -35,7 +35,7 @@ function bianca() {
 
 function sophie() {
     if (description.innerHTML === descriptionEmily || description.innerHTML === descriptionBianca || description.innerHTML === descriptionAlanis || description.innerHTML === descriptionAna) {
-        names.innerHTML = "Sophie";
+        names.innerHTML = "Véu Sophie";
         description.innerHTML = descriptionSophie;
         price.innerHTML = "R$ 78,00";
         image.src = "image/sophie-veil.jpeg";
@@ -44,7 +44,7 @@ function sophie() {
 
 function alanis() {
     if (description.innerHTML === descriptionEmily || description.innerHTML === descriptionBianca || description.innerHTML === descriptionSophie || description.innerHTML === descriptionAna) {
-        names.innerHTML = "Alanis";
+        names.innerHTML = "Véu Alanis";
         description.innerHTML = descriptionAlanis;
         price.innerHTML = "R$ 66,00";
         image.src = "image/alanis-veil.jpeg";
@@ -53,7 +53,7 @@ function alanis() {
 
 function ana() {
     if (description.innerHTML === descriptionEmily || description.innerHTML === descriptionBianca || description.innerHTML === descriptionSophie || description.innerHTML === descriptionAlanis) {
-        names.innerHTML = "Ana";
+        names.innerHTML = "Véu Ana";
         description.innerHTML = descriptionAna;
         price.innerHTML = "R$ 72,00";
         image.src = "image/ana-veil.jpeg";

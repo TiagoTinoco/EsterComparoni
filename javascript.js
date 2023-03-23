c5b68ec21a8134648beb517398ade7e4364301de
Tiago Tinoco <tiagotinocomartinsdossantos@gmail.com>
2023-03-23 00:19:39 -0300

    increment new structure json and auto javascript

--- javascript.js
+++ javascript.js
@@ -1,82 +1,33 @@
-let veilsListDiv = document.getElementsByClassName('list');
-console.log(veilsListDiv);
+let veilsListDiv = document.getElementsByClassName('list')[0];
+
+var names = document.getElementById('name');
+var description = document.getElementById('description');
+var price = document.getElementById('price');
+var image = document.getElementById('img');
 
 function fillList(){
-    VEILS.forEach(veilObject => {
+    VEILS.forEach((veilObject, index) => {
         let veilCardHTML = 
         `<a class="list-item" href="#img">
-            <div onclick="onSelectVeil(${veilObject.id})" class="thickness">
+            <div onclick="onSelectVeil(${index})" class="thickness">
                 <img src="${veilObject.image}" alt="Véu">
                 <h2>${veilObject.name}</h2>
             </div>
         </a>`;
         veilsListDiv.innerHTML += veilCardHTML;
-
     });
 }
-function onSelectVeil(id){
-   console.log(id);
+
+function onSelectVeil(index){
+    let veilsCard = document.getElementsByClassName('thickness')[index];
+    // TODO:(TiagoTinoco) resolve this problem!
+    // veilsCard.classList.add('selected');
+    // veilsCard.classList.remove('selected');
+
+    names.innerHTML = VEILS[index].name;
+    description.innerHTML = VEILS[index].description;
+    price.innerHTML = VEILS[index].price;
+    image.src = VEILS[index].image;
 }
 
 document.addEventListener("DOMContentLoaded", fillList);
-// var names = document.getElementById('name');
-// var description = document.getElementById('description');
-// var price = document.getElementById('price');
-// var image = document.getElementById('img');
-
-// var idEmily = document.getElementById('emily');
-// var idBianca = document.getElementById('bianca');
-// var idsophie = document.getElementById('sophie');
-// var idsophie = document.getElementById('alanis;')
-// var idsophie = document.getElementById('ana');
-
-// var descriptionEmily = "Um modelo clássico e elegante. Tecido de algodão e renda com 3,0cm de largura. Acompanha um porta véu com fita de cetim e detalhe em renda.";
-// var descriptionBianca = "Um modelo delicado e charmoso. Tecido de algodão e renda com 3,0 cm de largura. Acompanha um porta véu com fita de cetim e detalhe em renda."; 
-// var descriptionSophie = "Modelo floral e romântico. Tecido de algodão e renda com 5,2cm de largura. Acompanha um porta véu com fita de cetim e detalhe em renda.";
-// var descriptionAlanis = "Tecido de algodão e renda com 8cm de largura. Acompanha um porta véu com fita de cetim e detalhe em renda.";
-// var descriptionAna = "Tecido de algodão e renda com 5,8cm de largura. Acompanha um porta véu com fita de cetim e detalhe em renda.";
-
-// function emily() {
-//     if (description.innerHTML === descriptionBianca || description.innerHTML === descriptionSophie || description.innerHTML === descriptionAlanis || description.innerHTML === descriptionAna) {
-//         names.innerHTML = "Véu Emily";
-//         description.innerHTML = descriptionEmily;
-//         price.innerHTML = "R$ 60,00";
-//         image.src = "image/emily-veil.jpeg";
-//     }
-// }
-
-// function bianca() {
-//     if (description.innerHTML === descriptionEmily || description.innerHTML === descriptionSophie || description.innerHTML === descriptionAlanis || description.innerHTML === descriptionAna) {
-//         names.innerHTML = "Véu Bianca";
-//         description.innerHTML = descriptionBianca;
-//         price.innerHTML = "R$ 65,00";
-//         image.src = "image/bianca-veil.jpeg";
-//     }
-// }
-
-// function sophie() {
-//     if (description.innerHTML === descriptionEmily || description.innerHTML === descriptionBianca || description.innerHTML === descriptionAlanis || description.innerHTML === descriptionAna) {
-//         names.innerHTML = "Véu Sophie";
-//         description.innerHTML = descriptionSophie;
-//         price.innerHTML = "R$ 78,00";
-//         image.src = "image/sophie-veil.jpeg";
-//     }
-// }
-
-// function alanis() {
-//     if (description.innerHTML === descriptionEmily || description.innerHTML === descriptionBianca || description.innerHTML === descriptionSophie || description.innerHTML === descriptionAna) {
-//         names.innerHTML = "Véu Alanis";
-//         description.innerHTML = descriptionAlanis;
-//         price.innerHTML = "R$ 66,00";
-//         image.src = "image/alanis-veil.jpeg";
-//     }
-// }
-
-// function ana() {
-//     if (description.innerHTML === descriptionEmily || description.innerHTML === descriptionBianca || description.innerHTML === descriptionSophie || description.innerHTML === descriptionAlanis) {
-//         names.innerHTML = "Véu Ana";
-//         description.innerHTML = descriptionAna;
-//         price.innerHTML = "R$ 72,00";
-//         image.src = "image/ana-veil.jpeg";
-//     }
-// }

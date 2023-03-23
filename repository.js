f8a7f9e08424c1a20c6664125d76d99455ee5c2d
Tiago Tinoco <tiagotinocomartinsdossantos@gmail.com>
2023-03-22 22:55:56 -0300

    commit by victor

---
+++ repository.js
@@ -0,0 +1,37 @@
+const VEILS = [
+    {
+        id: 0,
+        name: 'Véu Emily',
+        description: 'Um modelo clássico e elegante. Tecido de algodão e renda com 3,0cm de largura. Acompanha um porta véu com fita de cetim e detalhe em renda.',
+        price: 60.00,
+        image: 'image/emily-veil.jpeg',
+    },
+    {
+        id: 1,
+        name: 'Véu Bianca',
+        description: 'Um modelo delicado e charmoso. Tecido de algodão e renda com 3,0 cm de largura. Acompanha um porta véu com fita de cetim e detalhe em renda.',
+        price: 65.00,
+        image: 'image/bianca-veil.jpeg',
+    },
+    {
+        id: 2,
+        name: 'Véu Alanis',
+        description: 'Tecido de algodão e renda com 8cm de largura. Acompanha um porta véu com fita de cetim e detalhe em renda.',
+        price: 66.00,
+        image: 'image/alanis-veil.jpeg',
+    },
+    {
+        id: 3,
+        name: 'Véu Ana',
+        description: 'Tecido de algodão e renda com 5,8cm de largura. Acompanha um porta véu com fita de cetim e detalhe em renda.',
+        price: 72.00,
+        image: 'image/ana-veil.jpeg',
+    },
+    {
+        id: 4,
+        name: 'Véu Sophie',
+        description: 'Modelo floral e romântico. Tecido de algodão e renda com 5,2cm de largura. Acompanha um porta véu com fita de cetim e detalhe em renda.',
+        price: 78.00,
+        image: 'image/sophie-veil.jpeg',
+    }
+]

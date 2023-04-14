4a911fd0a1e95627c21e005a4e306943601f5d22
Tiago Tinoco <tiagotinocomartinsdossantos@gmail.com>
2023-04-14 17:10:20 -0300

    new alterations

--- repository.js
+++ repository.js
@@ -2,25 +2,25 @@ const VEILS = [
     {
         name: 'Véu Emily',
         description: 'Tecido de algodão e renda com 3,0cm de largura. Acompanha um porta véu com fita de cetim e detalhe em renda.',
-        price: '60.00',
+        price: '60,00',
         image: 'image/emily-veil.jpeg',
     },
     {
         name: 'Véu Bianca',
         description: 'Tecido de algodão e renda com 3,0 cm de largura. Acompanha um porta véu com fita de cetim e detalhe em renda.',
-        price: '65.00',
+        price: '65,00',
         image: 'image/bianca-veil.jpeg',
     },
     {
         name: 'ESGOTADO',
         description: 'Tecido de algodão e renda com 8cm de largura. Acompanha um porta véu com fita de cetim e detalhe em renda.',
-        price: '66.00',
+        price: '66,00',
         image: 'image/alanis-veil.jpeg',
     },
     {
         name: 'Véu Ana',
         description: 'Tecido de algodão e renda com 5,8cm de largura. Acompanha um porta véu com fita de cetim e detalhe em renda.',
-        price: '72.00',
+        price: '72,00',
         image: 'image/ana-veil.jpeg',
     }
 ]
